276635ca8308ad6346a55c6074fd1dd6b06652db
darenT <darence.thong@gmail.com@users.noreply.github.com>
2022-02-26 00:16:23 -0800

    deployed on heroku

--- docs/settings.py
+++ docs/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3t0sc*qt&322&bri!wndbfmnls(1g8lj$f1m4utj@tg7+r#gag'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['afternoon-river-20074.herokuapp.com/']
 
